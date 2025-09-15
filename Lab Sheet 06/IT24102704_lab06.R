3e3d9d94c458dfd176c9ea5bd543c74f13936934
Daham Chinthaka <IT24102704@my.sliit.lk>
2025-09-15 19:32:45 +0530

    lab 6

---
+++ Lab Sheet 06/IT24102704_lab06.R
@@ -0,0 +1,16 @@
+#Exercise - lab6- IT24102704
+
+#1
+#i - Binomial Distribution
+  
+#ii - 
+1 - pbinom(46,50,0.85, lower.tail = TRUE)
+
+#2
+
+#i. - Number of  customers call per hour
+
+#ii - Possion Distribution
+
+#iii
+dpois(15,12)
