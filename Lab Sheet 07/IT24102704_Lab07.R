6503a25679fd6d08a3f76914925d6b31adf1fb1d
Daham Chinthaka <IT24102704@my.sliit.lk>
2025-09-16 21:08:06 +0530

    lab 7

---
+++ Lab Sheet 07/IT24102704_Lab07.R
@@ -0,0 +1,17 @@
+# Lab7_PS - IT24102704 - Exercise
+
+setwd("C:\\Users\\daham\\Desktop\\pS\\IT24102704")
+
+#1
+punif(25,min=0,max=40,lower.tail = TRUE) - punif(10,min=0,max=40,lower.tail = TRUE)
+
+#2
+pexp(2,rate = 0.33,lower.tail = TRUE)
+
+#3
+#i
+pnorm(130, mean = 100, sd= 15 , lower.tail=FALSE)
+
+#ii
+qnorm(0.95,mean = 100, sd= 15 , lower.tail=TRUE)
+
